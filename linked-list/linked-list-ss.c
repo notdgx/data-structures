2c4041c3e820690cbeca8096762af156b0bfbadd
notdgx <219498562+notdgx@users.noreply.github.com>
2026-09-04 19:05:39 +0530

    13 - Fixed the Simple Single Linked List

--- linked-list/linked-list-ss.c
+++ linked-list/linked-list-ss.c
@@ -29,6 +29,7 @@ int insert_at_beg(struct node ** beg,struct node ** end,int data){
         (*end) = new_node;
         return -1;
     }
+    // putting a new node nxy ptr to beg and theb beg = new node
     new_node->nxt_ptr = (*beg);
     (*beg) = new_node;
     
@@ -68,31 +69,30 @@ int insert_at_pos(struct node ** end,struct node ** beg,int data,int pos){
     new_node->data = data;
     new_node->nxt_ptr = NULL;
     
-    if ((pos==0) && (size ==0)){
+    if (size == 0){                
         (*beg) = new_node;
         (*end) = new_node;
         return -1;
     }
 
-    if ((pos == size)){
-        int tempdata;
-        struct node * temp = (*end);
-        
-        tempdata = temp->data; 
-        new_node->data = tempdata;
-        temp->data = data; 
-        
+    if (pos == 0){
+        new_node->nxt_ptr = (*beg);
+        (*beg) = new_node;
+        return 1;
+    }
+
+    if (pos == size){
+        (*end)->nxt_ptr = new_node;
         (*end) = new_node;
         return -2;
     }
     
     struct node * temp = (*beg);
-    for (n;n<=pos;n++){
+    for (n;n<pos - 1;n++){
         temp = temp->nxt_ptr;
     }
-    struct node * nxt = temp->nxt_ptr;
+    new_node->nxt_ptr = temp->nxt_ptr;
     temp->nxt_ptr = new_node;
-    new_node->nxt_ptr = nxt;
     return 1;
 
 }
@@ -129,11 +129,11 @@ int delete_at_end(struct node ** end,struct node ** beg){
         (*end) = NULL;
         return -1;
     }
-    struct node * temp = (*beg);
+
     struct node * pre = (*beg);
-    while(temp->nxt_ptr!=NULL){
-        pre = temp;
-        temp = temp->nxt_ptr;
+    while(pre->nxt_ptr!=(*end)){
+        // pre = temp;
+        pre = pre->nxt_ptr;
     }
 
     pre->nxt_ptr = NULL;
@@ -142,25 +142,38 @@ int delete_at_end(struct node ** end,struct node ** beg){
     return 1;
 }
 
+//so the thing if if size is 5 the
+// index rtange os 0 - 4 , if i wnart to add at 2 , then i need
+// the temp to hold the ptr of 1st indec becasue it have the next and next ; so i can insert the eleemnt 
+//at that poistion change the address of n-1 index and the new node containing the ttemp->nxt_ptr ; FOR deletion we 
+//also need the temp to has address of n-1 , temp will have to first get temp->nxtptr->nxtprt address and teh copy it to the temp nxtptr 
+
+
+// IMPLEEMTAION
+
+// but in fucntion impleemtion i did something weired insert utpo pre , delete upto the actual value then also trcked the pre 
+
 
 int delete_at_pos(struct node ** end,struct node ** beg,int pos){
     int n = 0; 
-    int size = fn_size((*beg),(*end)) - 1;
+    int size = fn_size((*beg),(*end));
     
     if (!((pos>=0) && (pos<=size))){return 0;}
     
-    if ((pos==0) && (size ==0)){
+    if ((size ==0)){
         return 0;
     }
     
-    if ((pos == size)){
-        delete_at_end(end,beg);
-        return -1;
+    if (pos == 0){
+        return (delete_at_beg(beg, end) == -1) ? -1 : 1;
+    }
+    if (pos == size - 1){
+        return (delete_at_end(beg, end) == -1) ? -1 : 1;
     }
     
     struct node * temp = (*beg);
     struct node * pre = (*beg);
-    for (n;n<=pos;n++){
+    for (n;n<pos;n++){
         pre=temp;
         temp=temp->nxt_ptr;
     }
@@ -171,7 +184,7 @@ int delete_at_pos(struct node ** end,struct node ** beg,int pos){
 
 
 int search(struct node * end,struct node * beg,int data){
-    if ((beg = NULL) && (end = beg)){
+    if ((beg == NULL) && (end == beg)){
         return -2;
     }
     int i = 0;
@@ -216,7 +229,7 @@ int sort(struct node **beg,struct node **end){
         temp->data=arr[i];
         temp = temp->nxt_ptr;
     }
-
+    free(arr);
     return 1;
 
 }
@@ -230,8 +243,8 @@ void show(struct node * beg,struct node * end){
     
     if ((end == beg) && (end->nxt_ptr == NULL)){
         printf("|b>  %3d  <e|",beg->data);
+        return;
     }
-    return;
     
     int i = 0;
     
@@ -330,7 +343,7 @@ int main (){
             printf("Enter the number insert at end : ");
             scanf("%d", &input_val);
 
-            out = insert_at_end(&beg,&end,input_val);
+            out = insert_at_end(&end,&beg,input_val);
             if (out == 1)
                 printf("%d inserted \n", input_val);
             else if (out == -1)
@@ -350,7 +363,7 @@ int main (){
                 printf("Linked List Reset \n");
             break;
         case 4:
-            out = delete_at_end(&beg,&end);
+            out = delete_at_end(&end,&beg);
             if (out == 1)
                 printf("Deleted value\n");
             else if (out == 0)
@@ -365,7 +378,7 @@ int main (){
             printf("Enter the position to insert  : ");
             scanf("%d", &pos);
 
-            out = insert_at_pos(&beg,&end,input_val,pos);
+            out = insert_at_pos(&end,&beg,input_val,pos);
             if (out == 1)
                 printf("%d inserted at %d\n", input_val,pos);
             else if (out == -1)
@@ -381,11 +394,13 @@ int main (){
             printf("\nEnter the position to delete : ");
             scanf("%d", &input_val);
 
-            out = delete_at_pos(&beg,&end,input_val);
+            out = delete_at_pos(&end,&beg,input_val);
             if (out == 1)
                 printf("%d Deleted \n", input_val);
             else if (out == -1)
                 printf("%d deleted the last position \n", input_val);
+            else if (out == -2)
+                printf("deleted the beg eleement \n");
             else
                 printf("Empty \n");
             break;
@@ -428,7 +443,7 @@ int main (){
             else
                 printf("Cant Clear \n");
             break;
-                break;
+
                 
                 case 12:
                 if (is_empty(beg,end) != 1){show(beg,end);}
@@ -439,7 +454,7 @@ int main (){
                 else
                     printf("Cant Sort \n");
                 break;
-                    break;
+
             case 0:
             printf("exitting ...\n");
             return 0;
